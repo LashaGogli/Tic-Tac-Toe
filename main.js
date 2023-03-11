20cef35cd452fe4b3063f50336911bd8ec3b44f1
Lasha <lashagoglidzelasha@gmail.com>
2023-03-11 12:51:46 +0400

    Quit Updated

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ let turnOfX = 0;
 let oTakesRound = document.querySelector(".oTakesRound");
 let xTakesRound = document.querySelector(".xTakesRound");
 
-
 divforX.addEventListener("click", function () {
     divforX.style.backgroundColor = "#A8BFC9";
     xShape.style.fill = "#1A2A33";
@@ -23,15 +22,26 @@ divforY.addEventListener("click", function () {
     turnOfX--;
 })
 
-secondPage = document.querySelector(".secondPage");
-firstPage = document.querySelector(".firstPage");
-newgamePlayer = document.querySelector(".newgamePlayer");
-vsplayer = document.querySelector(".vsplayer");
-forFirst = document.querySelector("#forFirst");
-forSecond = document.querySelector("#forSecond");
+let secondPage = document.querySelector(".secondPage");
+let firstPage = document.querySelector(".firstPage");
+let newgamePlayer = document.querySelector(".newgamePlayer");
+let vsplayer = document.querySelector(".vsplayer");
+let forFirst = document.querySelector("#forFirst");
+let forSecond = document.querySelector("#forSecond");
+let xScore = document.querySelector("#xScore");
+let scoreX = parseInt(localStorage.getItem("score")) || 0;
+xScore.innerHTML = scoreX;
+let oScore = document.querySelector("#oScore");
+let scoreO = parseInt(localStorage.getItem("score2")) || 0;
+oScore.innerHTML = scoreO;
+let tieScore = document.querySelector("#tieScore");
+let scoreTie = parseInt(localStorage.getItem("score3")) || 0;
+scoreTie.innerHTML = scoreTie;
+
 
 
 vsplayer.addEventListener("click", function () {
+
     firstPage.style.display = "none";
     secondPage.style.display = "block";
     if (turnOfX == 1) {
@@ -56,10 +66,8 @@ let secondContainer = document.querySelector(".secondContainer");
 let uperDiv = document.querySelector(".uperDiv");
 let playBoardDiv = document.querySelector(".playBoardDiv");
 let bottomDiv = document.querySelector(".bottomDiv");
-
-
-
-
+let xsvg = document.querySelector(".xsvg");
+let osvg = document.querySelector(".osvg");
 
 function checkWinningCondition(array) {
     if ((array[0] == "X" && array[1] == "X" && array[2] == "X") || (array[3] == "X" && array[4] == "X" && array[5] == "X") || (array[6] == "X" && array[7] == "X" && array[8] == "X") || (array[0] == "X" && array[4] == "X" && array[8] == "X") || (array[2] == "X" && array[4] == "X" && array[6] == "X") || (array[0] == "X" && array[3] == "X" && array[6] == "X") || (array[1] == "X" && array[4] == "X" && array[7] == "X") || (array[2] == "X" && array[5] == "X" && array[8] == "X")) {
@@ -74,8 +82,13 @@ function checkWinningCondition(array) {
         playBoardDiv.style.opacity = "0.5";
         bottomDiv.style.backgroundColor = "#000000";
         bottomDiv.style.opacity = "0.5";
-
-       
+        xsvg.style.backgroundColor = "#000000";
+        xsvg.style.opacity = "0.5";
+        osvg.style.backgroundColor = "#000000";
+        osvg.style.opacity = "0.5";
+        scoreX++;
+        localStorage.setItem("score", scoreX);
+        xScore.innerHTML = scoreX;
 
 
     } else if ((array[0] == "O" && array[1] == "O" && array[2] == "O") || (array[3] == "O" && array[4] == "O" && array[5] == "O") || (array[6] == "O" && array[7] == "O" && array[8] == "O") || (array[0] == "O" && array[4] == "O" && array[8] == "O") || (array[2] == "O" && array[4] == "O" && array[6] == "O") || (array[0] == "O" && array[3] == "O" && array[6] == "O") || (array[1] == "O" && array[4] == "O" && array[7] == "O") || (array[2] == "O" && array[5] == "O" && array[8] == "O")) {
@@ -90,10 +103,13 @@ function checkWinningCondition(array) {
         playBoardDiv.style.opacity = "0.5";
         bottomDiv.style.backgroundColor = "#000000";
         bottomDiv.style.opacity = "0.5";
-
-     
-
-
+        xsvg.style.backgroundColor = "#000000";
+        xsvg.style.opacity = "0.5";
+        osvg.style.backgroundColor = "#000000";
+        osvg.style.opacity = "0.5";
+        scoreO++;
+        localStorage.setItem("score2", scoreO);
+        oScore.innerHTML = scoreO;
 
     } else if (array.every(cell => cell != "")) {
         takesround.forEach(element => element.style.display = "flex");
@@ -109,14 +125,15 @@ function checkWinningCondition(array) {
         playBoardDiv.style.opacity = "0.5";
         bottomDiv.style.backgroundColor = "#000000";
         bottomDiv.style.opacity = "0.5";
-
-     
+        xsvg.style.backgroundColor = "#000000";
+        xsvg.style.opacity = "0.5";
+        osvg.style.backgroundColor = "#000000";
+        osvg.style.opacity = "0.5";
+        scoreTie++;
+        localStorage.setItem("score3", scoreTie);
+        tieScore.innerHTML = scoreTie;
     }
-
-
 }
-
-
 
 clickDivs.forEach((div, index) => {
     div.addEventListener("click", function () {
@@ -153,6 +170,40 @@ clickDivs.forEach((div, index) => {
 });
 
 
+let quitdiv = document.querySelector(".quitdiv");
+
+
+
+quitdiv.addEventListener("click", function () {
+    secondPage.style.display = "none";
+    firstPage.style.display = "flex";
+    takesround.forEach(element => element.style.display = "none");
+    scoreX = 0;
+    xScore.innerHTML = scoreX;
+    scoreO = 0;
+    oScore.innerHTML = scoreO;
+    scoreTie = 0;
+    tieScore.innerHTML = scoreTie;
+    switchtries = 0;
+    clickDivs.forEach(div => {
+        div.innerHTML = "";
+    });
+    board = ['', '', '', '', '', '', '', '', ''];
+
+    container.style.backgroundColor = "#1A2A33";
+    secondContainer.style.backgroundColor = "#1A2A33";
+    uperDiv.style.backgroundColor = "#1A2A33";
+    uperDiv.style.opacity = "1";
+    playBoardDiv.style.backgroundColor = "#1A2A33";
+    playBoardDiv.style.opacity = "1";
+    bottomDiv.style.backgroundColor = "#1A2A33";
+    bottomDiv.style.opacity = "1";
+    xsvg.style.backgroundColor = "#1A2A33";
+    xsvg.style.opacity = "1";
+    osvg.style.backgroundColor = "#1A2A33";
+    osvg.style.opacity = "1";
+
+})
 
 
 
